8cf434ba6d6957a41d209900318e5621effdbe00
risa-endo <167213224+risa-endo@users.noreply.github.com>
2024-08-15 17:55:14 +0900

    fairuwo

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $item_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($item_data);
+    ?>
+  </p>
+  
+</body>
+</html>
